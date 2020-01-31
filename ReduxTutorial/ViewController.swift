f92dcfe6e9e22bd797758cc4b95de11d0223cb99
bondan23 <bondan.ekoprasetyo@gmail.com>
2020-01-31 18:23:28 +0700

    Initial Commit

---
+++ ReduxTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReduxTutorial
+//
+//  Created by Bondan Prasetyo on 31/01/20.
+//  Copyright © 2020 Bondan Prasetyo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
